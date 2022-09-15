186eb45719c8e5904f70a91a74caca13dbef60ab
Satyajit Kuila <sanu123j@gmail.com>
2022-09-15 18:37:58 +0530

    cypress day-4

--- Project1/cypress/e2e/login.cy.ts
+++ Project1/cypress/e2e/login.cy.ts
@@ -14,7 +14,18 @@ describe('AllLogin test', () => {
 
 describe('Get Table data', () => {
     it('T1:user data on home page', function () {
+        loginPage.navigate('https://trytestingthis.netlify.app/')
+        //cy.get('tbody > :nth-child(2) > :nth-child(1)')
+        //cy.get('tbody>tr').eq()
+        // cy.get('tbody > :nth-child(2) > :nth-child(4)')
+        // .then(function(Field) {
+        // const Fieldtext = Field.text();
+        // expect(Fieldtext).to.equal("27");})
         
+    
 
     })
 })
+
+
+
